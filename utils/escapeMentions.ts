d1ca7cf80037dcea39982f03b58929f23fe00767
The-Best-Codes <bestcodes.official@gmail.com>
2025-05-21 13:15:38 -0500

    update

--- utils/escapeMentions.ts
+++ utils/escapeMentions.ts
@@ -3,12 +3,8 @@ export function escapeMentions(text: string): string {
 
   return (
     text
-      // Escape @everyone and @here
       .replace(/@everyone/g, "@\u200Beveryone")
       .replace(/@here/g, "@\u200Bhere")
-      // Escape user mentions
-      .replace(/<@!?(\d+)>/g, "<@\u200B$1>")
-      // Escape role mentions
-      .replace(/<@&(\d+)>/g, "<@&\u200B$1>")
+      .replace(/<@&(\d+)>/g, "<@&\u200B$1>") // Roles
   );
 }
