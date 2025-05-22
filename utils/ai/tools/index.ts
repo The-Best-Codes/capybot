9562c338a25f32ccd3fe7fe3f3d04f3066ce8289
The-Best-Codes <bestcodes.official@gmail.com>
2025-05-21 19:39:21 -0500

    set status removed

--- utils/ai/tools/index.ts
+++ utils/ai/tools/index.ts
@@ -2,10 +2,8 @@ import { getUserInfo } from "./getUserInfo";
 import { searchChannels } from "./searchChannels";
 import { searchRoles } from "./searchRoles";
 import { searchServerMembers } from "./searchServerMembers";
-import { setStatus } from "./setStatus";
 
 export const tools = {
-  [setStatus.name]: setStatus,
   [getUserInfo.name]: getUserInfo,
   [searchChannels.name]: searchChannels,
   [searchRoles.name]: searchRoles,
