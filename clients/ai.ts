a51c23b7132b4c4d3fb7919542d58872e0d6df98
The-Best-Codes <bestcodes.official@gmail.com>
2026-01-09 13:49:58 -0600

    model & prompt adjustments

--- clients/ai.ts
+++ clients/ai.ts
@@ -1,14 +1,12 @@
 import { openrouter } from "@openrouter/ai-sdk-provider";
 
-export const globalModel = openrouter(
-  "xiaomi/mimo-v2-flash:free" /*{
+export const globalModel = openrouter("xiaomi/mimo-v2-flash:free", {
   reasoning: {
     enabled: false,
     exclude: true,
     max_tokens: 0,
   },
-}*/,
-);
+});
 export const attachmentModel = openrouter(
   "nvidia/nemotron-nano-12b-v2-vl:free",
 );
