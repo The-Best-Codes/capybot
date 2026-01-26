0e679d5e7e046846486cc51afdf6f57bc12318de
The-Best-Codes <bestcodes.official@gmail.com>
2026-01-26 16:04:54 -0600

    fix model names

--- clients/ai.ts
+++ clients/ai.ts
@@ -1,6 +1,6 @@
 import { createOpenAICompatible } from "@ai-sdk/openai-compatible";
 
-export const heliconeAiProvider = createOpenAICompatible({
+export const heliconeProvider = createOpenAICompatible({
   name: "helicone",
   apiKey: process.env.HELICONE_AI_API_KEY,
   baseURL: "https://ai-gateway.helicone.ai",
@@ -8,8 +8,6 @@ export const heliconeAiProvider = createOpenAICompatible({
 });
 
 // export const globalModel = openrouter("xiaomi/mimo-v2-flash:free");
-export const globalModel = heliconeAiProvider("google/gemini-3-flash-preview");
+export const globalModel = heliconeProvider("gemini-3-flash-preview");
 // export const attachmentModel = openrouter("nvidia/nemotron-nano-12b-v2-vl:free");
-export const attachmentModel = heliconeAiProvider(
-  "google/gemini-2.5-flash-lite",
-);
+export const attachmentModel = heliconeProvider("gemini-2.5-flash-lite");
