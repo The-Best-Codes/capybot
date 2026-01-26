fb10cecdf0a2cf928d25a0e7c81125a03447cc43
The-Best-Codes <bestcodes.official@gmail.com>
2026-01-26 15:59:49 -0600

    migrate to helicone ai

--- clients/ai.ts
+++ clients/ai.ts
@@ -1,7 +1,15 @@
-import { openrouter } from "@openrouter/ai-sdk-provider";
+import { createOpenAICompatible } from "@ai-sdk/openai-compatible";
+
+export const heliconeAiProvider = createOpenAICompatible({
+  name: "helicone",
+  apiKey: process.env.HELICONE_AI_API_KEY,
+  baseURL: "https://ai-gateway.helicone.ai",
+  includeUsage: true,
+});
 
 // export const globalModel = openrouter("xiaomi/mimo-v2-flash:free");
-export const globalModel = openrouter("google/gemini-3-flash-preview");
-export const attachmentModel = openrouter(
-  "nvidia/nemotron-nano-12b-v2-vl:free",
+export const globalModel = heliconeAiProvider("google/gemini-3-flash-preview");
+// export const attachmentModel = openrouter("nvidia/nemotron-nano-12b-v2-vl:free");
+export const attachmentModel = heliconeAiProvider(
+  "google/gemini-2.5-flash-lite",
 );
